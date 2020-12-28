3decea4401fe6fd47bd083271e4f63b93a85f212
M4RC0N3 <marconeribeiro22@gmail.com>
2020-12-28 01:19:29 -0300

    pagina home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
+import React from "react";
+import logoUC from "./img/logoUC.svg";
+import desktop from "./img/desktop.svg";
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <img src={logoUC} alt="Logo do YouConverter" draggable="false"/>
+          <ul>
+            <li>Home</li>
+            <li>Conversor</li>
+            <li>Contador</li>
+            <li>Sobre</li>
+          </ul>
+        </nav>
       </header>
+      <section className="home">
+        <div className="chao"></div>
+        <img src={desktop} alt="Desktop"/>
+        <div className="card-container">
+          <div className="card-content">
+            <h1>YOU<span>CONVERTER</span></h1>
+            <p>
+              Esse é um site de converter frases e 
+              textos de minúsculo para maiúsculo e 
+              vice-versa. Além de possuir 
+              ferramenta que contabiliza o número 
+              de palavras e parágrafos de um texto.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
